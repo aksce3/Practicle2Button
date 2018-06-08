398ee978477cabd464802ef0bd5f4fcc57d1699a
aksce3 <aksce3@gmail.com>
2018-06-08 12:34:08 +0530

    Second Commit

--- app/src/main/java/com/example/akshayparmar/practicle2button/MainActivity.kt
+++ app/src/main/java/com/example/akshayparmar/practicle2button/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.akshayparmar.practicle2button
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Reference to the Button
+        val button = findViewById<Button>(R.id.button)
+
+        //Set on-click listener
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "You Click the Button", Toast.LENGTH_LONG).show()
+        }
     }
 }
