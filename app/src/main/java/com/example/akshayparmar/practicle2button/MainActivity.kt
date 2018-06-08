01180533291e6dda60ec1d3a7051cc70e29460a0
aksce3 <aksce3@gmail.com>
2018-06-08 14:31:46 +0530

    Third commit

--- app/src/main/java/com/example/akshayparmar/practicle2button/MainActivity.kt
+++ app/src/main/java/com/example/akshayparmar/practicle2button/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.akshayparmar.practicle2button
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,10 +17,35 @@ class MainActivity : AppCompatActivity() {
 
         //Reference to the Button
         val button = findViewById<Button>(R.id.button)
+        var textbox = findViewById<EditText>(R.id.editText)
+        var button1 = findViewById<Button>(R.id.button2)
+        var textview = findViewById<TextView>(R.id.textView6)
 
         //Set on-click listener
         button.setOnClickListener {
             Toast.makeText(applicationContext, "You Click the Button", Toast.LENGTH_LONG).show()
         }
+
+        fun showtext(){
+                if(textbox != null) {
+                    Toast.makeText(applicationContext, textbox.text, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        button1.setOnClickListener {
+            showtext()
+        }
+
+        textbox.setOnClickListener {
+               textview.setText(textbox.text)
+             }
+
+        textbox.addTextChangedListener()
+
     }
+}
+
+private fun TextView.addTextChangedListener() {
+       textView6.setText(textbox.text)
+    
 }
